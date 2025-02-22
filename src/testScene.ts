ef76358a8da03895629b55acff4728b0f7685dfa
Tonoplas909 <matis.nico123@gmail.com>
2025-02-22 17:14:52 +0100

    autoformat + deleted slope

--- src/testScene.ts
+++ src/testScene.ts
@@ -17,41 +17,31 @@ class TestScene {
         scene.collisionsEnabled = true;
 
         // light
-        var light1: HemisphericLight = new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
+        new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
+        
         // ground
-        var ground: Mesh = MeshBuilder.CreateGround("ground", { width: 20, height: 20 }, scene);
+        const ground: Mesh = MeshBuilder.CreateGround("ground", { width: 20, height: 20 }, scene);
 
         // Apply texture to ground
-        var groundMaterial: StandardMaterial = new StandardMaterial("groundMaterial", scene);
-        var groundTexture: Texture = new Texture("/assets/textures/blackPrototype.png", scene);
-        // set the texture scaling
-        groundTexture.uScale = 5
+        const groundMaterial: StandardMaterial = new StandardMaterial("groundMaterial", scene);
+        const groundTexture: Texture = new Texture("/assets/textures/blackPrototype.png", scene);
+        groundTexture.uScale = 5;
         groundTexture.vScale = 5;
         groundMaterial.diffuseTexture = groundTexture;
         ground.material = groundMaterial;
         ground.checkCollisions = true;
 
-
         // cube 1
-        var cube1: Mesh = MeshBuilder.CreateBox("cube1", { size: 2 }, scene);
+        const cube1: Mesh = MeshBuilder.CreateBox("cube1", { size: 2 }, scene);
         cube1.position.y = 1;
         cube1.position.x = 5;
-        var cubeMaterial: StandardMaterial = new StandardMaterial("cubeMaterial", scene);
+        const cubeMaterial: StandardMaterial = new StandardMaterial("cubeMaterial", scene);
         cubeMaterial.diffuseTexture = new Texture("/assets/textures/greenPrototype.png", scene);
         cube1.material = cubeMaterial;
         cube1.checkCollisions = true;
 
-        // plane incline
-        var incline: Mesh = MeshBuilder.CreateGround("incline", { width: 2, height: 2.8 }, scene);
-        incline.rotation.x = Math.PI / 4;
-        incline.position = new Vector3(5, 1, 2);
-        var inclineMaterial: StandardMaterial = new StandardMaterial("inclineMaterial", scene);
-        inclineMaterial.diffuseTexture = new Texture("/assets/textures/greenPrototype.png", scene);
-        incline.material = inclineMaterial;
-        incline.checkCollisions = true;
-
         // import class Player
-        var player: Player = new Player(canvas, scene);
+        const player: Player = new Player(canvas, scene);
 
         // call player movement
         player.movement();
